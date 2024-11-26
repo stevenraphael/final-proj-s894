679b08119b3058eb1296a6d1b03a131c0c7f53bd
Steven Raphael <stevenraphael44@gmail.com>
2024-11-26 17:17:10 -0500

    more infra

--- src/kmeans.cu
+++ src/kmeans.cu
@@ -9,9 +9,30 @@
 #include <vector>
 
 
+enum class Mode {
+    TEST,
+    BENCHMARK,
+};
+
+
+struct Scene {
+    int32_t dims;
+    int32_t n_points;
+    int32_t n_centroids;
+    std::vector<float> true_centroids;
+    std::vector<float> initial_centroids;
+    std::vector<float> features;
+};
+
+namespace kmeans {
+
 
 const int warp_size = 32;
 const int block_size = 4;
+
+size_t get_workspace_size(size_t n) {
+    return n;
+}
 
 __device__ void compute_clusters(
     int n,
@@ -69,21 +90,97 @@ __device__ void compute_centroids(
 
 __global__ void kmeans(
     int n,
-    
     int k,
     int d,
     float *points,
-    float *centroids,
+    float *initial_centroids,
+    float *output_centroids,
     uint32_t *centroid_map,
 ){
 }
 
-struct Scene {
-    int32_t dims;
-    int32_t n_points;
-    int32_t n_centroids;
-    std::vector<float> features;
+
+void launch_kmeans(
+    int n,
+    int k,
+    int d,
+    float *points,
+    float *initial_centroids,
+    float *output_centroids,
+    GpuMemoryPool &memory_pool
+){
+
+}
+
+
+}
+
+
+struct Results {
+    float average_squared_dist;
+    std::vector<float> centroids;
+    double time_ms;
 };
+
+
+Results run_config(Mode mode, Scene const &scene) {
+    auto points_gpu = GpuBuf<float>(scene.features);
+    auto centroids_gpu = GpuBuf<float>(scene.features);
+    auto memory_pool = GpuMemoryPool();
+
+
+    auto reset = [&]() {
+        CUDA_CHECK(
+            cudaMemset(points_gpu.data, 0, scene.features.size() * sizeof(float)));
+        CUDA_CHECK(
+            cudaMemset(centroids_gpu.data, 0, scene.true_centroids.size() * sizeof(float)));
+
+        memory_pool.reset();
+    };
+
+    auto f = [&]() {
+        kmeans::launch_render(
+            scene.n_points,
+            scene.n_centroids,
+            scene.dims,
+            points_gpu.data,
+            scene.initial_centroids.data,
+            centroids_gpu.data,
+            memory_pool);
+    };
+
+    reset();
+    f();
+
+
+    auto returned_centroids = std::vector<float>(scene.true_centroids.size(), 0.0f);
+     CUDA_CHECK(cudaMemcpy(
+        returned_centroids.data,
+        centroids_gpy.data,
+        scene.true_centroids.size() * sizeof(float),
+        cudaMemcpyDeviceToHost));
+
+    float squared_dist_sum = 0;
+
+    for(int i=0;i<scene.n_centroids;i++){
+        for(int j=0;j<scene.dims;j++){
+            squared_dist_sum += (scene.true_centroids[i*scene.dims+j]-returned_centroids[i*scene.dims+j])
+                                *(scene.true_centroids[i*scene.dims+j]-returned_centroids[i*scene.dims+j]);
+        }
+    }
+
+    float average_squared_dist = squared_dist_sum/scene.n_centroids;
+
+    double time_ms = benchmark_ms(1000.0, reset, f);
+
+    return Results{
+        average_squared_dist,
+        std::move(returned_centroids),
+        time_ms,
+    };
+
+}
+
 
 
 Scene gen_random(Rng &rng, int32_t dims, int32_t n_points, int32_t n_centroids){
@@ -117,6 +214,43 @@ Scene gen_random(Rng &rng, int32_t dims, int32_t n_points, int32_t n_centroids){
         }
     }
 
-    auto scene = Scene{n_points, n_centroids, dims, features};
+    auto initial_centroids = std::vector<float>();
+    for (int32_t i = 0; i < n_centroids*dims; i++) {
+        float z;
+        z = unif_0_1(rng);
+
+    
+        // float z = std::max(unif_0_1(rng), unif_0_1(rng));
+        initial_centroids.push_back(z);
+    }
+
+    auto scene = Scene{n_points, n_centroids, dims, true_centroids, initial_centroids, features};
+
+    return scene;
 
 }
+
+
+struct SceneTest {
+    std::string name;
+    Mode mode;
+    Scene scene;
+};
+
+int main(int argc, char const *const *argv) {
+    auto rng = std::mt19937(0xCA7CAFE);
+    auto scenes = std::vector<SceneTest>();
+    scenes.push_back(
+        {"test1", Mode::BENCHMARK, gen_random(rng, 10, 65536, 10)});
+    int32_t fail_count = 0;
+
+    int32_t count = 0;
+    for (auto const &scene_test : scenes) {
+        auto i = count++;
+        printf("\nTesting scene '%s'\n", scene_test.name.c_str());
+        auto results = run_config(scene_test.mode, scene_test.scene);
+    }
+
+}
+
+
